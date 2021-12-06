a4409d93a05d1fb97f8f3a9fb40a5928512ab5fd
Dsblima <dsbl@ecomp.poli.br>
2021-12-06 11:06:16 -0300

    Create account

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const {v4: uuidv4} = require("uuid")
 
 const app = express();
 
+const costumers = [];
 
-app.listen(3333)
+app.use(express.json());
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body;
+    const id = uuidv4();
+    console.log(name)    
+    costumers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
+});
+
+app.listen(3333);
