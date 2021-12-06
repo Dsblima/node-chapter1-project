4532beaa0e76d5cbc737be1548d9c56a6df7d6bc
Dsblima <dsbl@ecomp.poli.br>
2021-12-06 11:35:13 -0300

    Validating cpf already exists

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ const {v4: uuidv4} = require("uuid")
 
 const app = express();
 
-const costumers = [];
+const customers = [];
 
 app.use(express.json());
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
-    const id = uuidv4();
-    console.log(name)    
-    costumers.push({
+        
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({error: "Customer Already Exists!"});
+    }
+
+    customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
 
