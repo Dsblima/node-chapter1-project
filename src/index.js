72110443d42f74b1014ca2df3f9ebb043789e4b0
Dsblima <dsbl@ecomp.poli.br>
2021-12-25 12:44:44 -0300

    Create a withdraw

--- src/index.js
+++ src/index.js
@@ -20,6 +20,19 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+        
+    }, 0);
+    
+    return balance;
+}
+
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
     const {customer} = request;    
     return response.send(customer.statement);
@@ -27,22 +40,6 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
 
 
 app.use(express.json());
-
-app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
-    const {description, amount} = request.body;
-    const {customer} = request;
-
-    const statementOperation = {
-        description,
-        amount,
-        created_at: new Date(),
-        type: "credit" 
-    };
-
-    customer.statement.push(statementOperation);
-
-    return response.status(201).send();
-});
 
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
@@ -61,6 +58,43 @@ app.post("/account", (request, response) => {
         id: uuidv4(),
         statement: []
     });
+
+    return response.status(201).send();
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const {description, amount} = request.body;
+    const {customer} = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit" 
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({error: "Insufficient funds!"});
+    }
+
+    const statementOperation = {        
+        amount,
+        created_at: new Date(),
+        type: "debit",
+    };
+
+    customer.statement.push(statementOperation);
 
     return response.status(201).send();
 });
